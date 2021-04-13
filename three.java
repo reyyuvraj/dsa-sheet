d94b76f17a19744a801082c23f69dc4bd866e9bc
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-04-13 21:34:55 +0530

    String

---
+++ three.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class three
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		int i=0,len=s.length();
+		char c[] = new char[len];
+		int r[] = new int[26];
+		for(i=0;i<len;i++)
+			c[i]=s.charAt(i);
+		for(i=0;i<len;i++)
+			r[s.charAt(i)-97]++;
+		for(i=0;i<26;i++)
+			if(r[i]>1)
+				System.out.println((char)(i+97)+", count = "+ r[i]);
+	}
+}
